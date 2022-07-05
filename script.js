4ae68362a1de7fdd4637fefc006ede9d15d9f89a
suryam0801 <58582997+suryam0801@users.noreply.github.com>
2022-07-05 11:26:33 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    var specialElementHandler = {
+        "#editor":function(element,renderer) {
+            return true;
+        }
+    }
+
+    $("#cmd").click(function() {
+        var doc = new jsPDF();
+
+        doc.fromHTML($("#target").html(), 15, 15, {
+            "width":170,
+            "elementHandlers":specialElementHandler
+        });
+
+        doc.save("sample-file.pdf");
+    })
+});
